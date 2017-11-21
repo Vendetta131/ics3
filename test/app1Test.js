4385d7e41267b6274d7dbbcf6ccd3a716cdf0b0a
Denis Tsymbal <tsimbal1311@gmail.com>
2017-11-21 22:43:05 +0200

    test for app1.js

---
+++ test/app1Test.js
@@ -0,0 +1,28 @@
+const assert = require('chai').assert;
+// const sayHello = require('../app.js').sayHello;
+// const addNumbers = require('../app.js').addNumbers;
+const app = require('../app1');
+
+describe('App', function() {
+  describe('sayHello()', function() {
+    it('sayHello should return string', function() {
+      let result = app.sayHello();
+      assert.typeOf(result, 'string');
+    })
+
+    it('sayHello should return hello', function() {
+      let result = app.sayHello();
+      assert.equal(result, 'hello');
+    });
+  })
+  describe('addNumbers()', function() {
+    it('addNumbers should return number', function() {
+      let result = app.addNumbers(2, 3);
+      assert.typeOf(result, 'number');
+    })
+    it('addNumbers should be above 5', function() {
+      let result = app.addNumbers(3, 3);
+      assert.isAbove(result, 5);
+    })
+  })
+});
